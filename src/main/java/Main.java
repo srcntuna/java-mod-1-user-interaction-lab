a8e96d11e5e99da8664542fa5737f6508fe7434d
Sercan Tuna <ssercantuna@hotmail.com>
2022-06-23 13:33:36 -0400

    completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // your code here
+        System.out.print("Please enter your age:");
+         int age = 0;
+        try(Scanner scanner = new Scanner(System.in)){
+            age = scanner.nextInt();
+            if(age<0){
+                throw new Exception();
+            }
+
+        }catch(Exception e){
+             System.out.println("please type your age as in positive number format!!");
+             return;
+        }
+ if(age == 18){
+            System.out.println("You are 18, you are also allowed to enter the site!!");
+        }else if(age > 18){
+            System.out.println("You are allowed to enter the site!!");
+        }else{
+            if(age<10){
+                System.out.println("You are too young to enter the site!! You must be 18 or greater.");
+                return;
+            }
+            System.out.println("You are not allowed to enter the site, Sorry!");
+        }
+
     }
 }
